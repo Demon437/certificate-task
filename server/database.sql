fd51dd5025be6c64a2d4a0744c3f3381f3ce9b2b
Demon437 <namdevshanu206@gmail.com>
2025-06-16 12:52:18 +0530

    Updated: SQL file with latest changes

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS task_db;
 USE task_db;
 
--- Table: certificate_categories
+
 CREATE TABLE IF NOT EXISTS certificate_categories (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
--- Table: certificate_questions
+
 CREATE TABLE IF NOT EXISTS certificate_questions (
   id INT AUTO_INCREMENT PRIMARY KEY,
   question_text TEXT NOT NULL,
@@ -15,7 +15,6 @@ CREATE TABLE IF NOT EXISTS certificate_questions (
   FOREIGN KEY (category_id) REFERENCES certificate_categories(id)
 );
 
--- Sample data (optional)
 INSERT INTO certificate_categories (name) VALUES ('HTML'), ('CSS'), ('JavaScript');
 
 INSERT INTO certificate_questions (question_text, category_id) VALUES
